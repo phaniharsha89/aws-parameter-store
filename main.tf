0d03dad5f0969ebe825fa94c9f5921729e9958b7
phaniharsha89 <phaniharsha89@gmail.com>
2023-07-23 18:52:08 +0530

    sfsd

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_ssm_parameter" "params" {
   count= length(var.params)  
   name  = var.params[count.index].name
-  type  = var.params[count.index].string
+  type  = var.params[count.index].type
   value = var.params[count.index].value
 }
 
