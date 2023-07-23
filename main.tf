1d9f7b2542853666199c839975eec87014025567
phaniharsha89 <phaniharsha89@gmail.com>
2023-07-23 18:54:55 +0530

    sfsd

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ resource "aws_ssm_parameter" "params" {
 
 variable "params" {
 default =[
-{name= "roboshop.dev.frontend.catalogue_url", value="http://catalogue-dev.harshadevops.shop:8080/", type="string"},
-{name= "roboshop.dev.frontend.user_url", value="http://user-dev.harshadevops.shop:8080/", type="string"},
-{name= "roboshop.dev.frontend.cart_url", value="http://cart-dev.harshadevops.shop:8080/", type="string"},
-{name= "roboshop.dev.frontend.shipping_url", value="http://shipping-dev.harshadevops.shop:8080/", type="string"},
-{name= "roboshop.dev.frontend.payment_url", value="http://payment-dev.harshadevops.shop:8080/", type="string"},
+{name= "roboshop.dev.frontend.catalogue_url", value="http://catalogue-dev.harshadevops.shop:8080/", type="String"},
+{name= "roboshop.dev.frontend.user_url", value="http://user-dev.harshadevops.shop:8080/", type="String"},
+{name= "roboshop.dev.frontend.cart_url", value="http://cart-dev.harshadevops.shop:8080/", type="String"},
+{name= "roboshop.dev.frontend.shipping_url", value="http://shipping-dev.harshadevops.shop:8080/", type="String"},
+{name= "roboshop.dev.frontend.payment_url", value="http://payment-dev.harshadevops.shop:8080/", type="String"},
  ]
 }
