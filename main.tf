8080a77260b7ff967f24d44a5fddaac5c36f97a7
phaniharsha89 <phaniharsha89@gmail.com>
2023-07-23 18:40:17 +0530

    sfsd

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_ssm_parameter" "params" {
+  count= length(var.params)  
+  name  = var.params[count.index].name
+  type  = var.params[count.index].string
+  value = var.params[count.index].value
+}
+
+variable "params" {
+default =[
+{name= "roboshop.dev.frontend.catalogue_url", value="http://catalogue-dev.harshadevops.shop:8080/", type="string"},
+{name= "roboshop.dev.frontend.user_url", value="http://user-dev.harshadevops.shop:8080/", type="string"},
+{name= "roboshop.dev.frontend.cart_url", value="http://cart-dev.harshadevops.shop:8080/", type="string"},
+{name= "roboshop.dev.frontend.shipping_url", value="http://shipping-dev.harshadevops.shop:8080/", type="string"},
+{name= "roboshop.dev.frontend.payment_url", value="http://payment-dev.harshadevops.shop:8080/", type="string"},
+ ]
+}
